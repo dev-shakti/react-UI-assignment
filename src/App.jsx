f8b1368f87fc2d41039681d59562df45f7e83ff5
shakti sumant das <shakti.20k@gmail.com>
2025-03-31 15:09:48 +0530

    write a function to find first unique character

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,17 @@ import MainLayout from "./layout/MainLayout";
 import Homepage from "./pages/Homepage";
 
 function App() {
+  // function firstUniqueChar(str) {
+  //   for (let i = 0; i < str.length; i++) {
+  //     if (str.indexOf(str[i]) === str.lastIndexOf(str[i])) {
+  //       return str[i];  // Return the first unique character
+  //     }
+  //   }
+  //   return -1; //return -1 if all characters repeat
+  // }
+  
+  
+  // console.log(firstUniqueChar("leetcode")); 
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
